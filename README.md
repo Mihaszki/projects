7cd07071dcc3f2f501ef57efe3da9f76f0469dd8
Mihaszki <pawelmichalski9@gmail.com>
2022-11-30 00:31:47 +0100

    Upload repo

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# projects
+# About
 A simple portfolio website created with Tailwind CSS.
+
+## Setup
+* Install `npm install`
+* Build or watch 
+```
+npm run build
+npm run watch
+```
+* Run http server `npx http-server`
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    minHeight: {
+      '300': '300px',
+    },
+    maxWidth: {
+      '480': '480px',
+    },
+    minWidth: {
+      '480': '480px',
+    },
+    screens: {
+      sm: '480px',
+      md: '885px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      width: {
+        '480': '480px',
+      }
+    },
+  },
+  plugins: [],
+}
